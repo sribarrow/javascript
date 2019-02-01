8b6df62249732114437397a1694a35bd6102d6b2
sripriya <sribarrow@gmail.com>
2019-02-01 15:18:02 +0000

    after day 2

---
+++ app.js
@@ -0,0 +1,59 @@
+// declare variable
+
+var score;
+
+var livesLeft = 3;
+console.log(livesLeft);
+
+livesLeft=livesLeft -1;
+
+console.log(livesLeft);
+
+//constants
+
+const fname = 'John';
+
+
+// 
+
+var x = 5.3e8;
+
+console.log(x);
+
+var max = Number.MAX_VALUE;
+var min = Number.MIN_VALUE;
+
+console.log(max);
+console.log(min);
+
+var a = 4+9.3;
+console.log(a);
+
+var e = 8 ** 2;
+console.log(e);
+
+var i = Infinity;
+console.log(i);
+i = 4/0;
+console.log(i);
+
+var n = '3';
+
+n = 0/0;
+console.log(n);
+
+//power
+
+var p = Math.pow(2,40);
+console.log(p);
+
+//big number
+
+var z = Math.pow(2,1500);
+console.log(z);
+
+// parse a string
+
+var number= '4';
+console.log(parseInt(number));
+console.log(typeof(parseInt(number)));
---
+++ app1.js
@@ -0,0 +1,23 @@
+//string
+
+var fname = 'sripriya';
+
+var town = "Warlingham";
+
+console.log(fname);
+
+var message = "I am " + fname + ". I live in " + town;
+
+console.log(message);
+
+//quotes
+
+var str = 'I\'ve 2 children';
+
+console.log(str);
+
+var str = "I\'ve 2 children";
+
+console.log(str);
+
+
